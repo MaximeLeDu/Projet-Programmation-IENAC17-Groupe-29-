900d4639150b8bd679d547418de41b3e24540032
Maxime Le Du <34100056+MaximeLeDu@users.noreply.github.com>
2017-12-04 16:14:55 +0100

    Add files via upload

--- main_pong.py
+++ main_pong.py
@@ -1,9 +1,8 @@
 import numpy as np
-import torch
 import sys
 
 from random import random
-from ple import PLE
+from ple import PLE,matrice_coordonnées
 from ple.games.pong import Pong
 
 try:
@@ -12,26 +11,36 @@ try:
 	RANDOM_PARADIGM = int(sys.argv[2])
 	ALPHA = int(sys.argv[3])
 	GAMMA = int(sys.argv[4])
+	TRAINING = int(sys.argv[5])
+	LENGTH = int(sys.argv[6])
 
 except IndexError:
-	DISCRETIZATION=30
+	DISCRETIZATION=10
 	RANDOM_PARADIGM=0.6
 	ALPHA = 0.6
 	GAMMA = 0.6
+	TRAINING = True
+	LENGTH = 50000
+	
 
 class Agent():
 	def __init__(self, actions, states):
 		self.actions = actions
 		self.last_action = 0
 		self.last_state = None
+		
+		try:
+			self.rewards = matrice_coordonnées.uni_to_multi ( matrice_coordonnées.fichier_to_uni ("ple/Pong_reward.txt"))
+			print("Le fichier a pu être lu")
+		except:
+			print("Le fichier n'a pas pu être lu")
+			shape=()
+			for i in states:
+				shape+= (DISCRETIZATION+1,)
+			shape+=(len(actions),)
+			self.rewards=np.zeros(shape)
 
-		shape=()
-		for i in states:
-			shape+= (DISCRETIZATION+1,)
-		shape+=(len(actions),)
-		self.rewards=np.zeros(shape)
-
-	def pickAction(self, reward, state):
+	def training(self, reward, state):
 
 		if self.last_action!=0:
 			choix = random()
@@ -40,27 +49,25 @@ class Agent():
 			if choix > RANDOM_PARADIGM:
 		
 				matrix=self.rewards[state]
-				print(matrix,state,matrix.shape)
-				np.random.choice([action for action, value in enumerate(matrix) if value == np.max(matrix)])
-				print("choix conscient :",action)
+				action = np.random.choice([action for action, value in enumerate(matrix) if value == np.max(matrix)])
 
 			else:
 				action = np.random.randint(0, len(self.actions))
-				print(action)
-			
-			print("last state :", self.last_state, " last action :", self.last_action, "state :", state, "action :",action)
-			print(self.last_state+(self.last_action,), state+(action,))
 			self.rewards[self.last_state + (self.last_action,)]+=ALPHA*(reward + GAMMA*self.rewards[state+(action,)] - self.rewards[self.last_state + (self.last_action,)])
 			
 		#Initialization
 		else:
 			action = np.random.randint(0, len(self.actions))
-			print("last state :", self.last_state, " last action :", self.last_action, "state :", state, "action :",action)
 			
 		#Update of the last movements and states
 		self.last_state = state
 		self.last_action = action
-		print("act:",self.last_action)
+		return self.actions[action]
+		
+	def AI(self,reward,state):
+		
+		matrix=self.rewards[state]
+		action = np.random.choice([action for action, value in enumerate(matrix) if value == np.max(matrix)])
 		return self.actions[action]
 
 game=Pong()
@@ -73,7 +80,7 @@ display_screen = True
 
 reward = 0.0
 max_noops = 20
-nb_frames = 15000
+nb_frames = LENGTH
 
 
 p = PLE(game, fps=fps, frame_skip=frame_skip, num_steps=num_steps,
@@ -110,6 +117,12 @@ for f in range(nb_frames):
         p.reset_game()
 
     state_discretized=discretize(game)
-    action = agent.pickAction(reward, state_discretized)
+    if TRAINING :
+    	action = agent.training(reward, state_discretized)
+    else:
+    	action = agent.AI(reward,state_discretized)
     reward = p.act(action)
+    if f%100 ==0 :
+    	print(f)
 
+matrice_coordonnées.uni_to_fichier( matrice_coordonnées.multi_to_uni(agent.rewards),"ple/Pong_reward.txt")
